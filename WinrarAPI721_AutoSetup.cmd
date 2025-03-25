54de2d6932a436bdee59bc1280c84c2f97069bd2
danieldac1819 <danieldac1819@gmail.com>
2025-03-26 02:55:56 +0700

    Add files via upload

--- WinrarAPI721_AutoSetup.cmd
+++ WinrarAPI721_AutoSetup.cmd
@@ -1,3 +1,13 @@
-WinrarAPI721_AutoSetup.exe /autosetup 64
+WinrarAPI721_AutoSetup.exe /autosetup
 
 
+:: Cài đặt auto setup với thiết lập mặc định
+:: WinrarAPI721_AutoSetup.exe /autosetup
+
+:: Cài đặt auto setup với phiên bản 32-bit
+:: WinrarAPI721_AutoSetup.exe /autosetup 32
+
+:: Cài đặt auto setup với phiên bản 64-bit
+:: WinrarAPI721_AutoSetup.exe /autosetup 64
+:: truy cập vào facepage của chúng tôi để được hổ trợ cũng như tải xuống phiên bản mới nhất.
+:: WinrarAPI721_AutoSetup.exe /help
